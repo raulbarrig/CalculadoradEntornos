af0b6e0d958e37e784afbca77b6388e28fe4ba1c
raulbarriga <raul.barriga.robles@gmail.com>
2024-03-13 22:22:37 +0100

    commit-menu

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,62 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        boolean salida = true;
+        do {
+            System.out.println("--Calculadora--");
+            System.out.println("1.Suma");
+            System.out.println("2.Resta");
+            System.out.println("3.Multiplicacion");
+            System.out.println("4.Division");
+            System.out.println("0.Salir");
+            System.out.print("¿Que opcion quieres realizar?: ");
+            String opcion = sc.next();
+            opcion = opcion.trim();
+            switch (opcion) {
+                case "1":
+                    System.out.print("Introduce un numero: ");
+                    double num1 = sc.nextInt();
+                    System.out.print("Introduce otro numero: ");
+                    double num2 = sc.nextInt();
+                    System.out.println("La suma es: " + Calculadora.suma(num1, num2));
+                    break;
+                case "2":
+                    System.out.print("Introduce un numero: ");
+                    double num3 = sc.nextInt();
+                    System.out.print("Introduce otro numero: ");
+                    double num4 = sc.nextInt();
+                    System.out.println("La resta es: " + Calculadora.resta(num3, num4));
+                    break;
+                case "3":
+                    System.out.print("Introduce un numero: ");
+                    double num5 = sc.nextInt();
+                    System.out.print("Introduce otro numero: ");
+                    double num6 = sc.nextInt();
+                    System.out.println("La multiplicacion es: " + Calculadora.multiplicacion(num5, num6));
+                    break;
+                case "4":
+                    System.out.print("Introduce un numero: ");
+                    double num7 = sc.nextInt();
+                    System.out.print("Introduce otro numero: ");
+                    double num8 = sc.nextInt();
+                    if (num8 != 0) {
+                        System.out.println("La division es: " + Calculadora.division(num7, num8));
+                    } else {
+                        System.out.println("La division es: 0");
+                    }
+                    break;
+                case "0":
+                    System.out.println("Hasta otra");
+                    salida = false;
+                    break;
+                default:
+                    System.out.println("Error");
+            }
+        }while(salida);
     }
 }
