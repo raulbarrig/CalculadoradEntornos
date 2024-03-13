64af3382ec704faacb152b8bd6b2390de2d408e7
raulbarriga <raul.barriga.robles@gmail.com>
2024-03-13 22:28:38 +0100

    commit-resta

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,9 +1,16 @@
 public class Calculadora {
 
     static double suma;
+    static double resta;
+    static double multiplicacion;
+
     public static double suma(double num1,double num2){
         suma = (num1 + num2);
         return suma;
+    }
+    public static double resta(double num3,double num4){
+        resta = (num3 - num4);
+        return resta;
     }
 
 }
